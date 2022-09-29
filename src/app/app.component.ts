f542dc270eb2a3b8472d2555fa3daa20dc86e9d9
Ayush <ayush@159>
2022-09-29 18:47:44 +0530

    My First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-apps';
+  title = 'My Title';
+
+  count = 5;
+
+  number1 = 1;
+
+  number2 = 22;
+
+  add = () => {
+    let myresult = document.getElementById("demo") as HTMLInputElement;
+    myresult.value = String(this.number1 + this.number2);
+  }
+
+
+  
+  
+  listofnumbers = [1, 2, 3, 4, 5];
+
+  listoffruits = ['Apple', 'Mango', 'Banana', 'Grapes', 'Papaya'];
+
+  listofnames = ['Ayush Raj', 'Prandeep Chiring Phukan', 'Harsh Kumar Dwivedi'];
+
+  increment = () => {
+    this.count++;
+  }
+
+  decrement = () => {
+    this.count--;
+  }
+
 }
